51de44df49ea71f2ac798510ed2b16f5af4c4260
xu <42688005+isyziv@users.noreply.github.com>
2023-04-29 17:13:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -30,8 +30,8 @@ def create_csv_file(title,exchange_rates):
     month_year = date_time.strftime("%Y_%m")
     file_name = f"{month_year}.csv"
     file_path = os.path.join('data', file_name)
-    if not os.path.exists(file_path):
-        os.makedirs(file_path)
+    if not os.path.exists('data'):
+        os.makedirs('data')
     file_exists = os.path.isfile(file_path)
     with open(file_path, mode='a', newline='') as csv_file:
         fieldnames = title
